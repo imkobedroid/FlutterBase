5cac456ad00791317cf5d130da48274ef6cb6225
shihongdong <imkobedroid@gmail.com>
2019-09-16 20:55:32 +0800

    编写了状态管理的测试代码

---
+++ lib/flutter_9.dart
@@ -0,0 +1,69 @@
+//动画的使用
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(new FadeAppTest());
+}
+
+class FadeAppTest extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(title: "fade demo",
+        theme: new ThemeData(primarySwatch: Colors.blue,
+
+        ), home: new MyFadeTest(title: "fade demo"));
+  }
+}
+
+
+class MyFadeTest extends StatefulWidget {
+  MyFadeTest({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<StatefulWidget> createState() {
+    return new _MyFadeTest();
+  }
+}
+
+class _MyFadeTest extends State<MyFadeTest> with TickerProviderStateMixin {
+
+  AnimationController controller; //动画控制器
+  CurvedAnimation curve; //弯曲动画
+
+
+  @override
+  void initState() {
+    super.initState();
+    controller = new AnimationController(
+        duration: const Duration(milliseconds: 2000), vsync: this);
+    curve = new CurvedAnimation(parent: controller, curve: Curves.easeIn);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Center(
+          child: new Container(
+              child: new FadeTransition(
+                  opacity: curve,
+                  child: new FlutterLogo(
+                    size: 100.0,
+                  )))),
+      floatingActionButton: new FloatingActionButton(
+        tooltip: 'Fade',
+        child: new Icon(Icons.brush),
+        onPressed: () {
+          controller.forward();
+        },
+      ),
+    );
+  }
+}
